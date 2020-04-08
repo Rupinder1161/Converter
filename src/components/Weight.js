eba368a7ec6796a6018c580cfb4222c5b1a3b867
Gurpreet <gurpreet1161@gmail.com>
2020-04-08 20:34:07 +1200

    done the css thing

--- src/components/Weight.js
+++ src/components/Weight.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import './weight.css'
+import { FaEquals } from 'react-icons/fa';
+
+import TextField from '@material-ui/core/TextField';
 
 export default class Weight extends Component {
 
@@ -19,7 +22,32 @@ export default class Weight extends Component {
             <div className="MainDiv">
                 
                  <div className="CenterDiv">
-                     
+                     <h1>Weight Converter</h1>
+                     <div className="Divs">
+                                  <div className="EnterValue">
+                                                 <form  noValidate autoComplete="off" className = "forMain">
+                                                 <TextField id="outlined-basic" label="Your Name"  type="month" margin= "normal"   variant="outlined" color="secondary" style={{width:"80%"}} />
+                                                  </form>
+                                                        
+                                                   
+                                
+                                 </div>
+                                 <div className="SignValue">
+                                                 
+                                              <FaEquals size="2em"/>          
+                                                   
+                                
+                                 </div>
+                                 <div className ="GetValue"> 
+                                                 <form  noValidate autoComplete="off" className = "forMain">
+                                                 <TextField  id="outlined-basic" label="Your Name"  type="month" margin= "normal"   variant="outlined" color="secondary" style={{width:"80%"}} />
+                                                  </form>
+                                              
+
+                                 </div>
+
+
+                     </div>
 
                  </div>
 
