15033a1ac6f2af6a4de76b78923edd7589840e49
Gurpreet <gurpreet1161@gmail.com>
2020-04-08 19:14:43 +1200

    first commit

---
+++ src/components/Weight.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import './weight.css'
+
+export default class Weight extends Component {
+
+   constructor(props) {
+       super(props)
+   
+       this.state = {
+            
+       }
+   }
+   
+
+
+
+    render() {
+        return (
+            <div className="MainDiv">
+                
+                 <div className="CenterDiv">
+                     
+
+                 </div>
+
+
+
+            </div>
+        )
+    }
+}
